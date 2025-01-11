d132ebdcc6c30b5f364c6f13518442202f8ab9ff
44ll4l <bluesky5526@naver.com>
2025-01-11 22:14:24 +0900

    0111

--- src/test0111.java
+++ src/test0111.java
@@ -1,5 +1,15 @@
 public class test0111 {
     public static void main(String[] args) {
         System.out.println("오늘은 long byte 변수를 배웠읍니다.ㅋ");
+
+        byte var1 = -127;
+        byte var2 = 0;
+        byte var3 = 120;
+        //byte var4 = 128; 이거는 안되요ㅋ 범위뗌시
+
+        System.out.println("var1 = " + var1);
+        System.out.println("var2 = " + var2);
+        System.out.println("var3 = " + var3);
+
     }
 }
